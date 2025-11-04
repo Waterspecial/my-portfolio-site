752f3559a2a3291c1f4fa73170f3ed50692759f4
Waterspecial <MAC@MacBook-Pro.local>
2025-11-04 03:34:00 +0000

    working on the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./sections/Hero";
+import { Project } from "./sections/Project";
+import { About } from "./sections/About";
+import { Contact } from "./sections/Contact";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hello
+    <div className="min-h-screen">
+        <Navbar />
+        <Hero />
+      <Project />
+      <About />
+      <Contact />
     </div>
   );
 }
---
+++ app/sections/Project.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Project = () => {
+  return (
+    <section id='project'>Project</section>
+  )
+}
